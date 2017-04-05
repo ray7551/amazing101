1a21d62c308ea1210ffbdc46790c0c2c7cd7106f
ray7551 <ray7551@gmail.com>
2017-04-06 01:41:40 +0800

    fit

--- viewer/Viewer.js
+++ viewer/Viewer.js
@@ -3,14 +3,13 @@
  */
 class Viewer {
   constructor(canvas, thumbCanvas, gfx, thumbGfx) {
-    this.image = null;
+    this.imageElement = null;
     this.scale = 1;
     this.canvas = canvas; // main canvas
     this.thumbCanvas = thumbCanvas; // thumb canvas
     this.gfx = gfx;
     this.thumbGfx = thumbGfx;
 
-    this.draw();
     this.thumbGfx.drawRect(0, 0, 'rgb(255, 255, 255)', this.thumbCanvas.width, this.thumbCanvas.height);
 
     this.addEventListener();
@@ -29,108 +28,60 @@ class Viewer {
     let isDragging = false;
     let dragPosition = {};
     this.canvas.addEventListener('mousedown', (e) => {
-      isDragging = this.image && this.inImageRect({x: e.offsetX, y: e.offsetY});
+      isDragging = this.imageElement && this.inImageRect({x: e.offsetX, y: e.offsetY});
       dragPosition = {x: e.offsetX, y: e.offsetY};
     });
     this.canvas.addEventListener('mousemove', (e) => {
-      if(!isDragging) {
-        return;
-      }
+      if (!isDragging) return;
+
       let translateX = (e.offsetX - dragPosition.x) / this.gfx.scale.x;
       let translateY = (e.offsetY - dragPosition.y) / this.gfx.scale.y;
       this.gfx.ctx.translate(translateX, translateY);
       // this.gfx.ctx.translate(e.offsetX - dragPosition.x, e.offsetY - dragPosition.y);
       // Util.debounce(() => this.draw(), 100)();
-      // this.draw();
+      this.drawOnMove && this.draw();
       // this.startAnimation();
       dragPosition = {x: e.offsetX, y: e.offsetY};
     });
     this.canvas.addEventListener('mouseup', () => {
+      if (!isDragging) return;
+
       this.draw();
       isDragging = false;
     });
     this.canvas.addEventListener('mouseleave', () => {
+      if (!isDragging) return;
+
       this.draw();
       isDragging = false;
     });
   }
 
-  _initThumb() {
-    if (this.image === null) {
-      return;
-    }
-    let w = this.image.width, h = this.image.height,
-      cw = this.thumbCanvas.width, ch = this.thumbCanvas.height,
-      tan = h / w, cTan = ch / cw;
-    this.thumbSw = this.image.width;
-    this.thumbSh = this.image.height;
-    // calc where to draw img in the thumb canvas
-    // if tan <= cTan, then fit to thumb canvas width,
-    // otherwise, fit to thumb canvas height
-    if (tan <= cTan) {
-      this.thumbDw = cw;
-      this.thumbDh = cw * tan;
-      this.thumbDx = 0;
-      this.thumbDy = (ch - this.thumbDh) / 2;
-    } else {
-      this.thumbDh = ch;
-      this.thumbDw = ch / tan;
-      this.thumbDx = (cw - this.thumbDw) / 2;
-      this.thumbDy = 0;
-    }
-    this.thumbScale = this.thumbDw / w;
-    this.thumbGfx.drawImage(this.image, 0, 0, this.thumbSw, this.thumbSh, this.thumbDx, this.thumbDy, this.thumbDw, this.thumbDh);
-    this._updateThumbRect();
-  }
-
-  _updateThumbRect() {
-    let lineWidth = 2,
-      x = this.thumbScale * this.sx + this.thumbDx,
-      y = this.thumbScale * this.sy + this.thumbDy,
-      width = this.thumbScale * this.image.width,
-      height = this.thumbScale * this.image.height;
-    clog('update thumb rect', x, y, width, height, lineWidth);
-    this.thumbGfx.strokeInnerRect(x, y, 'red', width, height, lineWidth);
-  }
-
-  addImage(img) {
-    img.addEventListener('load', () => {
-      this.image = img;
-      this.draw(0, 0, img.width, img.height, 0, 0);
-      this._initThumb();
-    });
-  }
-
   // todo: if not need, just draw part of image by controlling sx, sy, sw, sh
-  draw(sx = 0, sy = 0, sw, sh, dx = 0, dy = 0) {
-    this.gfx.background('rgb(255, 255, 255)');
-    // this.gfx.grid();
-
-    if (!this.image) {
+  draw(sx = 0, sy = 0, sw, sh) {
+    if (!this.imageElement) {
       return;
     }
+
+    this.gfx.background('rgb(255, 255, 255)');
+    // this.gfx.background('gray');
+    // this.gfx.grid();
 
     this.sx = sx;
     this.sy = sy;
-    this.imagePosition = {x: dx, y: dy};
-    this.sw = sw || this.image.width;
-    this.sh = sh || this.image.height;
-    let canvas = this.canvas;
+    this.sw = this.imageElement.width;
+    this.sh = this.imageElement.height;
+    clog('this.image.width > this.canvas.width', this.imageElement.width > this.canvas.width);
+    clog('sx, sy', this.sx, this.sy);
+    clog('sw, sh', this.sw, this.sh);
 
-    // Put img at left top corner and resize image to make it all visible.
-    // Calc the destination imageWidth and imageHeight it should be
-    if (this.sw <= canvas.width && this.sh <= canvas.height) {
-      this.imageWidth = this.sw;
-      this.imageHeight = this.sh;
-    } else if (this.sh / this.sw >= canvas.height / canvas.width) {
-      this.imageHeight = canvas.height;
-      this.imageWidth = this.sw * canvas.height / this.sh;
-    } else {
-      this.imageWidth = canvas.width;
-      this.imageHeight = this.sh * canvas.width / this.sw;
-    }
+    let fit = this.fitImage(this.sw, this.sh, this.canvas.width, this.canvas.height);
+    this.imagePosition = {x: fit.position.x, y: fit.position.y};
+    this.imageWidth = fit.size.width;
+    this.imageHeight = fit.size.height;
+
     // todo: add loading notice before image really show up
-    this.gfx.drawImage(this.image, this.sx, this.sy, this.sw, this.sh,
+    this.gfx.drawImage(this.imageElement, this.sx, this.sy, this.sw, this.sh,
       this.imagePosition.x, this.imagePosition.y, this.imageWidth, this.imageHeight);
     // this.gfx.strokeInnerRect(200, 200, 'red', 200, 200, 2);
   }
@@ -145,10 +96,9 @@ class Viewer {
     let zoomStep = isZoomIn ? Viewer.zoomStep : -Viewer.zoomStep;
     let nextScale = this.gfx.scale.x * (1 + zoomStep);
     let nextWidth = nextScale * this.imageWidth;
-    
-    if (nextScale >= Viewer.zoomMax || nextScale <= Viewer.zoomMin
-      || nextWidth <= Viewer.renderedWidthMin * this.image.width
-      || nextWidth >= Viewer.renderedWidthMax * this.image.width) {
+
+    if (nextWidth <= Viewer.renderedWidthMin * this.imageElement.width
+      || nextWidth >= Viewer.renderedWidthMax * this.imageElement.width) {
       return;
     }
     let centerPosition = this.inImageRect(mousePosition) ? mousePosition : this.imageCenterPoint;
@@ -156,10 +106,89 @@ class Viewer {
     this.gfx.clear();
     this.gfx.zoom(zoomStep, centerPosition, () => this.draw());
   }
-  
+
+  _initThumb() {
+    if (this.imageElement === null) {
+      return;
+    }
+    let w = this.imageElement.width, h = this.imageElement.height,
+      cw = this.thumbCanvas.width, ch = this.thumbCanvas.height,
+      fit = this.fitImage(w, h, cw, ch, Viewer.fitMode.fillCanvas);
+    this.thumbPosition = {x: fit.position.x, y: fit.position.y};
+    this.thumbResize = fit.size.width / w;
+
+    this.thumbGfx.drawImage(this.imageElement, 0, 0, w, h,
+      this.thumbPosition.x, this.thumbPosition.y, fit.size.width, fit.size.height);
+    this._updateThumbRect();
+  }
+
+  _updateThumbRect() {
+    // let lineWidth = 2, imageOriginPoint = this.gfx.ctx.transformPoint(0, 0),
+    //   leftTopPoint;
+    //
+    // if(imageOriginPoint.x > 0 && imageOriginPoint.y > 0) {
+    //   leftTopPoint = Util.deepAssign({}, imageOriginPoint);
+    // } else {}
+    //
+    // let x = this.thumbResize * leftTopPoint.x + this.thumbDx,
+    //   y = this.thumbResize * leftTopPoint.y + this.thumbDy,
+    //   width = this.thumbResize * this.imageElement.width,
+    //   height = this.thumbResize * this.imageElement.height;
+    // this.thumbGfx.strokeInnerRect(x, y, 'red', width, height, lineWidth);
+  }
+
+  openImage(img) {
+    img.addEventListener('load', () => {
+      this.imageElement = img;
+      this.draw();
+      this._initThumb();
+    });
+  }
+
+  /**
+   * Put img at canvas center and resize image to make it all visible.
+   * Calc the destination imageWidth and imageHeight it should be
+   * @param imageWidth
+   * @param imageHeight
+   * @param canvasWidth
+   * @param canvasHeight
+   * @param {Symbol} [fitMode=Viewer.fitMode.center]
+   */
+  fitImage(imageWidth, imageHeight, canvasWidth, canvasHeight, fitMode = Viewer.fitMode.center) {
+    let imageRatio = imageHeight / imageWidth,
+      canvasRatio = canvasHeight / canvasWidth;
+    let ret = {
+      position: {x: 0, y: 0},
+      size: {width: imageWidth, height: imageHeight}
+    };
+    if (fitMode === Viewer.fitMode.center && this.sw <= canvasWidth && this.sh <= canvasHeight) {
+      ret.size.width = imageWidth;
+      ret.size.height = imageHeight;
+    } else if (imageRatio >= canvasRatio) {
+      ret.size.height = canvasHeight;
+      ret.size.width = canvasHeight / imageRatio;
+    } else {
+      ret.size.width = canvasWidth;
+      ret.size.height = canvasWidth * imageRatio;
+    }
+    ret.position = {
+      x: (canvasWidth - ret.size.width) / 2,
+      y: (canvasHeight - ret.size.height) / 2
+    };
+    return ret;
+  }
+
+  /**
+   * if image larger than 10000 x 10000, don't draw when mouse moving
+   */
+  get drawOnMove() {
+    return this.imageElement && this.imageElement.width * this.imageElement.height < 1e8;
+  }
+
   get imageLeftTopPoint() {
     return this.gfx.ctx.transformPoint(this.imagePosition);
   }
+
   get imageCenterPoint() {
     let width = this.imageWidth * this.gfx.scale.x;
     let height = this.imageHeight * this.gfx.scale.y;
@@ -181,15 +210,14 @@ class Viewer {
       && mousePosition.y >= topLeft.y && mousePosition.y <= topLeft.y + height;
   }
 
-  /**
-   * Drag and drop to move image
-   */
-  pan() {
-
-  }
 }
 Viewer.zoomStep = .1;
-Viewer.zoomMax = 25;
-Viewer.zoomMin = .001;
 Viewer.renderedWidthMin = .01;
-Viewer.renderedWidthMax = 10;
+Viewer.renderedWidthMax = 8;
+Viewer.fitMode = {
+  // if image is smaller than canvas, enlarge it to fill canvas
+  fillCanvas: Symbol(),
+  // if image is smaller than canvas, do not enlarge it,
+  // just place it at center of canvas
+  center: Symbol()
+};
--- viewer/index.js
+++ viewer/index.js
@@ -7,13 +7,12 @@ let clog = function (...args) {
 
 console.info('Please run it under latest chrome');
 document.addEventListener('DOMContentLoaded', function () {
-  /* @var canvas HTMLCanvasElement*/
   let canvas = document.querySelector('#paper');
   let thumbCanvas = document.querySelector('#thumb');
   let viewer = new Viewer(canvas, thumbCanvas, new Gfx(canvas, new Transform2D()), new Gfx(thumbCanvas));
-  const demoImages = ['./img/man.jpg', './img/small.jpg', './img/body.jpg', './img/railway.png', './img/fractal.jpg', './img/100.jpg',
-    './img/grid.jpg'];
+  const demoImages = ['./img/man.jpg', './img/small.jpg', './img/body.jpg', './img/railway.png', './img/fractal.jpg', './img/100.jpg', './img/15852x12392.jpg',
+    './img/grid.jpg', './img/10000x7328.jpg', './img/phanfone_eye.jpg'];
   let img = new Image();
-  img.src = demoImages[6];
-  viewer.addImage(img);
+  img.src = demoImages[7];
+  viewer.openImage(img);
 });
