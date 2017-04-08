872bb42aff3e18bad43e69b2825e2b1dcc1af168
ray7551 <ray7551@gmail.com>
2017-04-09 00:34:58 +0800

    add dot files

---
+++ viewer/.eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true,
+    "mocha": true
+  },
+  "globals": {
+    // "require": true,
+    // "chrome": true
+  },
+  "extends": "eslint:recommended",
+  // "parser": "babel-eslint",
+  "parserOptions": {
+    "ecmaVersion": 8, // or 2017
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "classes": true
+    },
+  },
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "warn",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
+  }
+};
